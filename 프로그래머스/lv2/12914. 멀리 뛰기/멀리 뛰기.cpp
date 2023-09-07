39b135fc9698a2bafe6cdcf3fb82a137e6358f3c
belocked <55683323+belocked@users.noreply.github.com>
2023-09-07 17:23:42 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    long long arr[2001];
+    arr[1]=1;
+    arr[2]=2;
+    if(n>2)
+    {
+        for(int i=3;i<=n;i++)
+            arr[i]=(arr[i-2]+arr[i-1])%1234567;
+    }
+    return arr[n];
+}
