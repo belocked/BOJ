600a9d9a503ab4b72845f743e7420fa73143f364
belocked <55683323+belocked@users.noreply.github.com>
2023-09-08 16:05:25 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 224 ms, Memory: 11184 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+vector<int> v[100001];
+int visited[100001];
+int result[100001];
+int cnt=0;
+void dfs(int r)
+{
+	if(visited[r])
+		return;
+	cnt++;
+	visited[r]=1;
+	result[r]=cnt;
+	for(int i=0;i<v[r].size();i++)
+		dfs(v[r][i]);
+}
+int main()
+{
+	int n,m,r;
+	cin>>n>>m>>r;
+	for(int i=0;i<m;i++)
+	{
+		int a,b;
+		cin>>a>>b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	for(int i=1;i<=n;i++)
+		sort(v[i].rbegin(),v[i].rend());
+	dfs(r);
+	for(int i=1;i<=n;i++)
+	{
+		cout<<result[i]<<'\n';
+	}
+}
