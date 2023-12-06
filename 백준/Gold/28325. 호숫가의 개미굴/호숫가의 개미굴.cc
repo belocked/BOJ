16871cb9ff8763c1b5b9d073416967cbb3fae448
belocked <55683323+belocked@users.noreply.github.com>
2023-12-06 16:43:23 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 192 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
@@ -7,36 +7,39 @@ int main()
 	int n;
 	cin>>n;
 	long long int tmp;
-	long long int sum = 0;
-	int start = 0, pre;
+	long long int sum0 = 0;
+	long long int sum1 = 0;
+	int pre[2]={0,1};
 	cin>>tmp;
-	sum+=tmp;
-	if(tmp==0)
-	{
-		start = 1;
-		sum++;
-	}
-	pre = start;
+	sum0+=tmp;
+	sum1++;
 	for(int i=1;i<n;i++)
 	{
 		cin>>tmp;
-		sum+=tmp;
-
 		if(tmp!=0)
 		{
-			pre=0;
+			pre[0]=0;
+			pre[1]=0;
+			sum0+=tmp;
+			sum1+=tmp;
 			continue;
 		}
-		if(pre ==1)
-			pre = 0;
-		else
+		if(pre[0]==0)
 		{
-			pre = 1;
-			sum++;
+			sum0++;
+			pre[0]=1;
 		}
+		else
+			pre[0]=0;
+		if(pre[1]==0)
+		{
+			sum1++;
+			pre[1]=1;
+		}
+		else
+			pre[1]=0;
 	}
-	if(start==1&&pre==1)
-		sum--;
-	cout<<sum<<endl;
+	sum1-=pre[1];
+	cout<<max(sum0,sum1)<<endl;
 	
 }
